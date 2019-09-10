5ee2287b826a73dd0c42121f8e018e0fa77d1449
fastndead <asmanumbetov@gmail.com>
2019-09-10 18:00:00 +0400

    Fixes

--- tasks/10_remove-whitespaces/remove-whitespaces.js
+++ tasks/10_remove-whitespaces/remove-whitespaces.js
@@ -1,8 +1,4 @@
 export function trimWhiteSpaces(str) {
-<<<<<<< HEAD
   const result = str.replace(/ /g, '');
   return result;
-=======
-  return str.split(' ').join('');
->>>>>>> 9a99949c5f7851e58d6f4aed49ae863c99a53a6f
 }
