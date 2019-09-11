7a20d998cec432b5c4ed122033f0e14de44bbd4f
fastndead <asmanumbetov@gmail.com>
2019-09-11 10:02:10 +0400

    Task 18

---
+++ tasks/18_humanize-format/humanize-format.js
@@ -0,0 +1,16 @@
+export function humanizeFormat(number) {
+  if (!number) {
+    return '';
+  }
+  const stringNumber = number.toString();
+  if (stringNumber.endsWith('1') && !stringNumber.endsWith('11')) {
+    return stringNumber.concat('st');
+  }
+  if (stringNumber.endsWith('2') && !stringNumber.endsWith('12')) {
+    return stringNumber.concat('nd');
+  }
+  if (stringNumber.endsWith('3') && !stringNumber.endsWith('13')) {
+    return stringNumber.concat('rd');
+  }
+  return stringNumber.concat('th');
+}
